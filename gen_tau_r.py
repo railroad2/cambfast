f2deb85c0c49b4ab51881f9f081273a263bb82c9
Kyungmin Lee <phynbook@gmail.com>
2020-06-02 15:14:58 +0900

    commit for github

--- gen_tau_r.py
+++ gen_tau_r.py
@@ -9,6 +9,7 @@ lmax = 50
 npts = 100
 
 cf = cambfast.CAMBfast()
+cf.ini_file = '../ini/planck_2018.ini'
 cf.add_parameter('tau', 0.05, 0.02, 0.08, npts, fixed=False)
 cf.add_parameter('r', 0.00, -0.3, 0.3, npts, fixed=False)
 cf.generate_interp(lmax, CMB_unit='muK')
