48b6d2593b77fb8a30ac5ad90a4df5b52828210f
Kyungmin Lee <phynbook@gmail.com>
2020-10-23 09:09:03 +0900

    update gen_tau_r.py

--- gen_tau_r.py
+++ gen_tau_r.py
@@ -5,14 +5,14 @@ import numpy as np
 import cambfast
 from cambfast import get_spectrum_camb
 
-lmax = 40
-npts = 30
+lmax = 50
+npts = 200
 
 cf = cambfast.CAMBfast()
-cf.ini_file = '../ini/planck_2018.ini'
-cf.add_parameter('As', 2.0e-9, 1.7e-9, 2.3e-9, npts, fixed=False)
-cf.add_parameter('tau', 0.05, 0.02, 0.12, npts, fixed=False)
-cf.add_parameter('r', 0.05, -0.3, 0.3, npts, fixed=False)
+cf.inifile = '../ini/planck_2018.ini'
+cf.add_parameter('tau', 0.05, 0.003, 0.12, npts, fixed=False)
+cf.add_parameter('r', 0.05, -0.3, 0.3, npts, fixed=True)
 cf.generate_interp(lmax, CMB_unit='muK')
-cf.write_funcs(f'As_tau_r_lmax{lmax}_npts{npts}.npz')
+#cf.write_funcs(f'As_tau_r_lmax{lmax}_npts{npts}.npz')
+cf.write_funcs(f'tau_lmax{lmax}_npts{npts}.npz')
 
